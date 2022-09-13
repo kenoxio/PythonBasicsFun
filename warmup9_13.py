09e5a85ee81eb47f21e7a6bb895f4611ecb65b91
kenoxio <nickbillnicole@protonmail.com>
2022-09-13 12:46:26 -0700

    warmup task

---
+++ warmup9_13.py
@@ -0,0 +1,8 @@
+# -------------------------------- warmup task 9/13
+#what are the three kinds of programming errors? can you give an example of each?
+#trace the output displayed by the following program when the data entered are 12 for m and 0 for n:
+m = int(input("enter an interger> "))
+n = int(input("enter an interger> "))
+m = m + 5
+n = 3 * n
+print("m =", m, "\\n =", n)
