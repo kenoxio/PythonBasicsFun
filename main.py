d5899b6a00652894175c7152cf48c5bc1b16bfaa
kenoxio <nickbillnicole@protonmail.com>
2022-09-13 12:44:38 -0700

    warmup task

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 # this is a one line comment askldjfaskldj
 # a comment is used to document your code
 # comments are ignored by python
@@ -44,12 +46,43 @@ print(type(x))
 # PEMDAS - Parenthesis(), exponents**, multiplication*, division (/, //, %), addition+, subtraction-.
 # +, -
 # * is multiplicaion
-print(4*5)
+print(4 * 5)
 # / is a floating point division (like "normal" division)
-print(5/2)
+print(4 / 12)
 # // is integer division (is the whole number result of floating point division)
-print(5//2)
+print(4 // 12)
 # % is the modulus operator (is the remainder of interger division)
-print(5 % 2)
+print(9 % 5)
 # ** is the exponentiation operator (power)
-print(5**2)
+print(5 ** 2)
+
+
+# warm up
+# print(3.0 % 1)
+# print (3 / 0)
+# print(2 ** 4 ** (2/4))
+print (math.pow(5,2))
+print("Enter your favorite number: ")
+fav_num = input()
+print(type(fav_num))
+print("Your favorite number doubled is", 2 * fav_num)
+# we often need to convert between types
+# type conversion
+
+# DECIMAL FORMATTING
+# there a LOTS of ways to do this.
+print(math.pi)
+print("%.2f" %(math.pi))
+print("{:.2f}".format(math.pi))
+print(f"{math.pi:.2f}")
+print(round(math.pi, 2))
+
+#conditionals AKA if statements
+#if some condition (boolean condition) is true
+#then execute some code
+x = 5
+if x == 5: # == is the equality operator
+    print("hello")
+    # python uses indentation (1 tab = 4 spaces)
+    # to group code together into "blocks"
+    # like {} C/C++
